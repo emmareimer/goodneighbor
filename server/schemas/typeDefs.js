fce97b45d0500707508fafa5656cd53456616f4e
emma@beinproximity.com <emma@beinproximity.com>
2022-05-25 19:09:13 -0600

    added to typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -10,6 +10,10 @@ const typeDefs = gql`
         address: String
         posted_tasks: [Task]
         claimed_tasks: [Task]
+        city: String
+        state: String
+        latitude: Int
+        Longitude: Int
     }
 
     type Task {
@@ -22,14 +26,24 @@ const typeDefs = gql`
         completed_at: String
         open: Boolean
         contactless: Boolean
+        city: String!
+        state: String!
+        latitude: Int
+        longitude: Int
     }
 
     type Query {
         tasks: [Task]
+        city: [Task]
+        state: [Task]
+        latitude: [Task]
+        longitude: [Task]
+        user: [User]
     }
 
     type Mutation {
         updateTask(id: ID, completed_by: String, completed_at: String, open: Boolean): Task
+        updateUser(id: ID, posted_tasks: String, claimed_tasks: String, address: String, city: String, state: String): User
     }
 `;
 
