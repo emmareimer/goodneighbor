68a2e47950fb0201666895a77ee287fd16afb723
emma@beinproximity.com <emma@beinproximity.com>
2022-06-10 21:46:03 -0600

    removed console log in client/auth.js

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -12,7 +12,6 @@ class AuthService {
   loggedIn() {
     // Checks if there is a saved token and it's still valid
     const token = this.getToken();
-    console.log(token);
     return !!token && !this.isTokenExpired(token); // handwaiving here
   }
 
